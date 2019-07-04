70f9b09ed28bc1e5ba2679efdc0cc3016ce60e62
TWQ1765 <www.taaadaaa@qq.com>
2019-07-04 18:19:52 +0800

    Created the <project_name> project

---
+++ test/test_DualSlopeADC_test.c
@@ -0,0 +1,16 @@
+#include "unity.h"
+#include "DualSlopeADC_test.h"
+#include "stm32f1xx_it.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_DualSlopeADC_test_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement DualSlopeADC_test");
+}
